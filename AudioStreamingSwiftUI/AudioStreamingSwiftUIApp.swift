4d597540093009c1d967ff97840bbf0dddab7798
Fachri Febrian <fachripaul@gmail.com>
2025-02-26 16:01:05 +0700

    Initial Commit

---
+++ AudioStreamingSwiftUI/AudioStreamingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioStreamingSwiftUIApp.swift
+//  AudioStreamingSwiftUI
+//
+//  Created by Fachri Febrian on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioStreamingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioStreamingSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioStreamingSwiftUI
+//
+//  Created by Fachri Febrian on 26/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
